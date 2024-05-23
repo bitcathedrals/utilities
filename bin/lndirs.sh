76f9758003d8f8ca5aa21bb23340f823b7bbaca0
Michael Mattie <codermattie@runbox.com>
2024-05-23 11:57:49 -0700

    (feat): add to lndirs.sh hard/soft links and recursion [lndirs.sh]

--- bin/lndirs.sh
+++ bin/lndirs.sh
@@ -1,13 +1,33 @@
 #! /usr/bin/env bash
 
+dry='false'
+
+if [[ $1 == '-dry' ]]
+then
+  dry='true'
+  shift
+fi
+
 target=$1
 shift
 
 for source in $@
 do
-  for entry in $(ls $source)
+  for entry in $(ls ${source}/**/* | sort)
   do
     normalized=$(readlink -f $source/$entry)
     echo >/dev/stderr "linking $entry : $normalized -> $target"
+
+    if [[ -d $entry ]]
+    then
+      mkdir $target/$entry
+    else
+      if [[ $SOFT == 'true' ]]
+      then
+        ln -s $normalized $target/$entry
+      else
+        ln $normalized $target/$entry
+      fi
+    fi
   done
 done
